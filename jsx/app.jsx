ee801d9fa2bf3302238cce49fa4d527477e82e6b
Eric Gelinas <eric@standardpixel.com>
2016-02-06 22:14:46 -0800

    get routes working

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link, IndexRoute } from 'react-router';
+import createHistory from 'history/lib/createHashHistory';
+
 import 'normalize.css/normalize.css';
 import '../sass/app.scss';
 
+import {HomePage} from './home-page.jsx';
+import {DetailPage} from './detail-page.jsx';
+
+// Opt-out of persistent state, not recommended.
+const history = createHistory({
+  queryKey: false
+});
+
 class App extends React.Component {
   render() {
-    return <h1>Hello</h1>
+    let view = null;
+    let zip = parseInt(this.props.params.zipCode);
+
+    if (!zip || zip.toString().length !== 5 || typeof zip !== 'number') {
+      view = (<HomePage />);
+    } else {
+      view = (<DetailPage zipCode={this.props.params.zipCode} />);
+    }
+    return (
+      <div>
+        {view}
+      </div>
+    )
   }
 }
 
-ReactDOM.render(<App />, document.querySelector("#app"));
+ReactDOM.render((
+  <Router history={history}>
+    <Route path="/" component={App}>
+      <IndexRoute component={HomePage}/>
+      <Route path=":zipCode(94110)" component={DetailPage}/>
+    </Route>
+  </Router>
+), document.querySelector("#app"));
---
+++ jsx/home-page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Link from 'react-router';
+import '../sass/home-page.scss';
+
+export class HomePage extends React.Component {
+  render() {
+    return <h1>Hello</h1>
+  }
+}
